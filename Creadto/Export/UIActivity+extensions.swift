05ec3cf2758dcb2ca450da2d3c801c2a7c109cae
sangjin-hash <lsj9873@ajou.ac.kr>
2022-11-07 23:12:04 +0900

    ObserveStatus implemented

--- Creadto/Export/UIActivity+extensions.swift
+++ Creadto/Export/UIActivity+extensions.swift
@@ -11,7 +11,7 @@ import Alamofire
 import UniformTypeIdentifiers
 
 final class ExportActivity : UIActivity {
-    private let apiURL : String = "http://192.168.219.102:3000"
+    private let apiURL : String = "http://192.168.219.128:3000"
     var mainController = MainController()
     
     override class var activityCategory: UIActivity.Category { return .share }
@@ -29,9 +29,8 @@ final class ExportActivity : UIActivity {
         let mimeType = filePath?.getMimeType()
         let fileName = filePath!.path.components(separatedBy: "/").last!
         
-        do {
+        do{
             let fileData = try? Data(contentsOf: filePath!)
-            
             fileUpload(
                 fileData: fileData ?? Data(),
                 fileName: fileName,
@@ -54,19 +53,100 @@ final class ExportActivity : UIActivity {
             progress in
             print(progress.fractionCompleted * 100)
         })
-        .response{
+        .responseJSON {
             response in
-            if let data = response.data{
-                let path = FileManager.default.urls(for: .documentDirectory,
-                                                    in: .userDomainMask)[0].appendingPathComponent("myFile.ply")
-                try! data.write(to: path)
-                print("저장 성공")
-                
-            } else{
-                print("Network Something went wrong")
+            switch response.result {
+            case .success(let value) :
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    let userData = try JSONDecoder().decode(userData.self, from: data)
+                    print("userData = \(userData)")
+                    
+                    switch(userData.Status){
+                    case "Idle", "Received", "Loaded":
+                        sleep(5)
+                        self.observeStatus()
+                    case "Meshed" :
+                        if let _data = userData.Data {
+                            let path = FileManager.default.urls(for: .documentDirectory,
+                                                                in: .userDomainMask)[0].appendingPathComponent("myFile.ply")
+                            try! _data.write(to: path)
+                            print("Save the file")
+                        } else{
+                            print("Not save the file")
+                        }
+                        
+                    default :
+                        print("default")
+                    }
+                    
+                } catch {
+                    print("response error")
+                }
+            case .failure(let error) :
+                print(error)
             }
         }
-        
+    }
+    
+    private func observeStatus(){
+        guard let url = URL(string: apiURL) else { return }
+        AF.request(url,
+                   method: .post,
+                   parameters: ["Status" : "check"],
+                   encoding: URLEncoding.default,
+                   headers: ["Content-Type" : "application/octet-stream"])
+        .validate(statusCode: 200..<300)
+        .responseJSON { response in
+            switch response.result {
+            case .success(let value) :
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    let userData = try JSONDecoder().decode(userData.self, from: data)
+                    print("userData2 = \(userData)")
+                    
+                    while(true) {
+                        if(userData.Status == "Meshed"){
+                            self.fileDownload()
+                            break
+                        }
+                        else{
+                            sleep(5)
+                            self.fileDownload()
+                        }
+                    }
+                } catch {
+                    print("response error")
+                }
+            case .failure(let error) :
+                print(error)
+            }
+        }
+    }
+    
+    private func fileDownload(){
+        guard let url = URL(string: apiURL) else { return }
+        AF.request(url,
+                   method: .post,
+                   parameters: nil,
+                   encoding: URLEncoding.default,
+                   headers: ["Content-Type" : "application/octet-stream"])
+        .validate(statusCode: 200..<300)
+        .response{ response in
+            switch response.result {
+            case .success :
+                let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("myFile.ply")
+                if let _data = response.data{
+                    try! _data.write(to: path)
+                    print("Save file 성공")
+                }else{
+                    print("Data is nil")
+                }
+            case .failure(let error):
+                print("Error: ", error)
+            }
+            
+        }
     }
 }
 
@@ -84,4 +164,9 @@ extension URL {
         }
         return "application/octet-stream"
     }
+}
+
+struct userData : Codable {
+    let Status : String
+    let Data : Data?
 }
